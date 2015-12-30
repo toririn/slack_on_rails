e092b584071a965ed596654d17011a1172cbdbb0
toririn <toririn.paftako@gmail.com>
2015-12-30 17:48:45 +0900

    文字列中の改行をviewで適用させるためhelperを実装

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  # 文字列中の改行文字を改行として出力させるメソッド
+  # e.g. "hello\nworld" => "hello<br />world" 
+  def br(text)
+    text = h(text).gsub(/(\r\n?)|(\n)/, "<br />")
+    text.html_safe
+  end
 end
