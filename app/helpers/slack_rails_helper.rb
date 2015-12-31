018a9d37ae804904cbb05a46c5e4348f1cc01112
toririn <toririn.paftako@gmail.com>
2015-12-31 12:32:22 +0900

    画面の表示に関する処理をhelperに抽出

--- app/helpers/slack_rails_helper.rb
+++ app/helpers/slack_rails_helper.rb
@@ -10,9 +10,28 @@ module SlackRailsHelper
   # Slackの検索結果からテーブルに表示する内容を抽出する。
   # e.g.["CH0003", "general", "UH0001", "Rails君", "チャットの内容", "http://〜〜〜"]
   def extract_for_table(search_results)
+    return [] if search_results["messages"].nil?
     search_results["messages"]["matches"].map do |res|
       [res["channel"]["id"], res["channel"]["name"], res["user"], res["username"], res["text"], res["permalink"]]
-    end
+    end.sort { |a, b| a[5] <=> b[5] }
+  end
+
+  def options_channel_list(channel_list)
+    channel_list.map do |channel|
+     [channel[0], channel[0]] 
+    end.unshift(["全部", "all"])
+  end
+
+  def options_user_list(user_list)
+    user_list.map do |user|
+     [user[0], user[0]] 
+    end.unshift(["全部", "all"])
+  end
+
+  def options_reaction_list(reaction_list)
+    reaction_list.map do |reaction|
+     [reaction[0], reaction[0]]
+    end.unshift(["全部", "all"])
   end
 
   # Slackの検索結果を全て表示させる。デバッグ用。
