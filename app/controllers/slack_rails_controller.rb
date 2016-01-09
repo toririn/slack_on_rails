2ec9ea5ff558f1d0a7d91eb8957f82d0fbdb053e
toririn <toririn.paftako@gmail.com>
2016-01-09 16:42:19 +0900

    ユーザ名とチャンネル名の表示対応のため

--- app/controllers/slack_rails_controller.rb
+++ app/controllers/slack_rails_controller.rb
@@ -89,6 +89,16 @@ class SlackRailsController < ApplicationController
   def set_slack_markdown_processor
     @slack_markdown_processor = SlackMarkdown::Processor.new(
       asset_root: 'https://assets.github.com/images/icons/',
+      on_slack_user_id: -> (uid) {
+        user_list = set_user_list
+        user_name = user_list.find {|user| user[1] == uid }[0]
+        return {url: "#{TEAM_DIRECTORY_URL}/#{user_name}", text: user_name }
+      },
+      on_slack_channel_id: -> (uid){
+        channel_list = set_channel_list
+        channel_name = channel_list.find {|channel| channel[1] == uid }[0]
+        return { url: '/', text: channel_name }
+      },
     )
   end
 
@@ -99,10 +109,10 @@ class SlackRailsController < ApplicationController
   end
 
   def set_channel_list
-    @channel_list = Service::SlackRails.channel_list
+    @channel_list ||= Service::SlackRails.channel_list
   end
 
   def set_user_list
-    @user_list = Service::SlackRails.user_list
+    @user_list ||= Service::SlackRails.user_list
   end
 end
--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -3,3 +3,4 @@ module Constants
 end
 SLACK_API_TOKEN = ENV['SLACK_API_TOKEN']
 RESULTS_EMPTY_MESSAGE = "条件に一致するチャットが見つかりませんでした。\n再度条件を変更して検索してください。"
+TEAM_DIRECTORY_URL = ENV['SLACK_URL'] + "/team"
