b3185fbebfa5186661e7c8d2010f12d77368b285
toririn <toririn.paftako@gmail.com>
2016-02-01 06:38:08 +0900

    【実装】fluentdによるログ集計を実装

--- Gemfile
+++ Gemfile
@@ -52,6 +52,7 @@ gem 'momentjs-rails'
 gem 'bootstrap3-datetimepicker-rails'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
+gem 'fluent-logger'
 
 # Use Unicorn as the app server
 # gem 'unicorn'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  Fluent::Logger::FluentLogger.open(nil, host: "localhost", port: 24224)
   before_action :authenticate_user!
+  before_action :fluent_post if Rails.env.production?
 
   def authenticate_user!
     return redirect_to root_path if session[:user].blank?
@@ -12,5 +14,13 @@ class ApplicationController < ActionController::Base
       reset_session
       redirect_to root_path
     end
+  end
+
+  def fluent_post
+    Fluent::Logger.post("slack_on_rails.access", {
+      "url" => request.fullpath,
+      "time" => Time.zone.now.to_s,
+      "user" => session[:user].try(:[], "name")
+    })
   end
 end
