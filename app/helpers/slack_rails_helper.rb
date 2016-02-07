906e8236f89ac800a3a34d842a9dafde5f46c1b2
toririn <toririn.paftako@gmail.com>
2016-02-08 00:30:53 +0900

    【エラー対策】helperでのエラーをハンドリング

--- app/helpers/slack_rails_helper.rb
+++ app/helpers/slack_rails_helper.rb
@@ -4,34 +4,33 @@ module SlackRailsHelper
   def channel_name_list(search_results)
     search_results["messages"]["matches"].map do |res|
       [res["channel"]["id"], res["channel"]["name"]]
-    end
+    end rescue []
   end
 
   # Slackの検索結果からテーブルに表示する内容を抽出する。
   # e.g.["CH0003", "general", "UH0001", "Rails君", "チャットの内容", "http://〜〜〜"]
   def extract_for_table(search_results)
-    return [] if search_results["messages"].nil?
     search_results["messages"]["matches"].map do |res|
       [res["channel"]["id"], res["channel"]["name"], res["user"], res["username"], res["text"], res["permalink"], res["ts"]]
-    end.sort { |a, b| a[6] <=> b[6] }
+    end.sort { |a, b| a[6] <=> b[6] } rescue []
   end
 
   def options_channel_list(channel_list)
     channel_list.map do |channel|
      [channel[0], channel[0]] 
-    end.unshift(["全部", "all"])
+    end.unshift(["全部", "all"]) rescue []
   end
 
   def options_user_list(user_list)
     user_list.map do |user|
      [user[0], user[0]] 
-    end.unshift(["全部", "all"])
+    end.unshift(["全部", "all"]) rescue []
   end
 
   def options_reaction_list(reaction_list)
     reaction_list.map do |reaction|
      [reaction[0], reaction[0]]
-    end.unshift(["全部", "all"])
+    end.unshift(["全部", "all"]) rescue []
   end
 
   # Slackの検索結果を全て表示させる。デバッグ用。
