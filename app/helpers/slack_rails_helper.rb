e0efc60f48b870a304d83f659af68d7c10b4c974
toririn <toririn.paftako@gmail.com>
2016-02-15 00:05:20 +0900

    初期化処理の変更。高速化のため

--- app/helpers/slack_rails_helper.rb
+++ app/helpers/slack_rails_helper.rb
@@ -7,14 +7,6 @@ module SlackRailsHelper
     end rescue []
   end
 
-  # Slackの検索結果からテーブルに表示する内容を抽出する。
-  # e.g.["CH0003", "general", "UH0001", "Rails君", "チャットの内容", "http://〜〜〜"]
-  def extract_for_table(search_results)
-    search_results["messages"]["matches"].map do |res|
-      [res["channel"]["id"], res["channel"]["name"], res["user"], res["username"], res["text"], res["permalink"], res["ts"]]
-    end.sort { |a, b| a[6] <=> b[6] } rescue []
-  end
-
   def options_channel_list(channel_list)
     channel_list.map do |channel|
      [channel[0], channel[0]] 
@@ -32,12 +24,4 @@ module SlackRailsHelper
      [reaction[0], reaction[0]]
     end.unshift(["全部", "all"]) rescue []
   end
-
-  # Slackの検索結果を全て表示させる。デバッグ用。
-  def debug_for(search_results)
-    search_results["messages"]["matches"].map do |res|
-     p res
-    end
-  end
-
 end
