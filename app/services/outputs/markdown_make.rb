b3b1c1835043bfeedee89aaa5b359655fc2c3857
toririn <toririn.paftako@gmail.com>
2016-02-21 22:34:11 +0900

    【修正】読み込んだデータを利用

--- app/services/outputs/markdown_make.rb
+++ app/services/outputs/markdown_make.rb
@@ -78,26 +78,14 @@ class Outputs::MarkdownMake
   end
 
   def self.slack_markdown_processor
-    @@slack_markdown_processor ||= SlackMarkdown::Processor.new(
-      asset_root: 'https://assets.github.com/images/icons/',
-      on_slack_user_id: -> (uid) {
-        user_list = set_user_list
-        user_name = user_list.find {|user| user[1] == uid }[0]
-        return {url: "#{TEAM_DIRECTORY_URL}/#{user_name}", text: user_name }
-      },
-      on_slack_channel_id: -> (uid){
-        channel_list = set_channel_list
-        channel_name = channel_list.find {|channel| channel[1] == uid }[0]
-        return { url: '/', text: channel_name }
-      },
-    )
+    SLACK_MARKDOWN_PROCESSOR
   end
 
   def self.set_channel_list
-    @@channel_list ||= slack.channel_list
+    SLACK_CHANNEL_LIST
   end
 
   def self.set_user_list
-    @@user_list ||= slack.user_list
+    SLACK_USER_LIST
   end
 end
