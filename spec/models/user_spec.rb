a0faf42cf336b878521150ac56f435a87da20e2f
toririn <toririn.paftako@gmail.com>
2016-04-03 11:42:04 +0900

    【追加】クラスメソッドのテストケース追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,9 +24,26 @@ RSpec.describe User, type: :model do
   end
 
   describe "クラスメソッド" do
-    context "【正常系】"
-    it "user_id と user_name  の変換が出来ている" do
-      expect(User.convert(id: user.id)).to eq(user.name)
+    context "【正常系】" do
+      it "id で name への変換が出来ている" do
+        current_user = user
+        expect(User.convert(id: current_user.id)).to eq(current_user.name)
+      end
+      it "name で id の変換が出来ている" do
+        current_user = user
+        expect(User.convert(name: current_user.name)).to eq(current_user.id)
+      end
+    end
+
+    context "【異常系】" do
+      it "id に一致する name がないときに not found を返す" do
+        current_user = user
+        expect(User.convert(id: current_user.id.next)).to eq("not found")
+      end
+      it "name に一致する id がないときに not found を返す" do
+        current_user = user
+        expect(User.convert(name: current_user.name.chop)).to eq("not found")
+      end
     end
   end
 end
